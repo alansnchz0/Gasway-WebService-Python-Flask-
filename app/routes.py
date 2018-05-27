a5b79a8d6f252c077125dec884b7b22c695a4cb1
alansnchz0 <alan.snchz0@gmail.com>
2018-05-26 23:58:08 -0500

    updated file location

--- app/routes.py
+++ app/routes.py
@@ -17,13 +17,13 @@ def download_files():
     print('downloading file')
     url = 'https://publicacionexterna.azurewebsites.net/publicaciones/prices'
     r = requests.get(url)
-    with open('/home/gasway/www/Gasway-WebService-Python-Flask-/app/static/prices.xml', 'wb') as f:
+    with open('/home/gasway/Gasway-WebService-Python-Flask-/app/static/prices.xml', 'wb') as f:
         f.write(r.content)
     print('file downloaded')
     print('downloading file')
     url = 'https://publicacionexterna.azurewebsites.net/publicaciones/places'
     r = requests.get(url)
-    with open('/home/gasway/www/Gasway-WebService-Python-Flask-/app/static/places.xml', 'wb') as f:
+    with open('/home/gasway/Gasway-WebService-Python-Flask-/app/static/places.xml', 'wb') as f:
         f.write(r.content)
     print('file downloaded')
 
@@ -57,7 +57,7 @@ def getStations():
     places_id = list()
     stations = list()
 
-    places_tree = ET.parse('/home/gasway/www/Gasway-WebService-Python-Flask-/app/static/places.xml')
+    places_tree = ET.parse('/home/gasway/Gasway-WebService-Python-Flask-/app/static/places.xml')
     places = places_tree.getroot()
     for place in places:
         data = place.find('location')
@@ -78,7 +78,7 @@ def getStations():
             stations.append(s)
             places_id.append(place.attrib)
 
-    prices_tree = ET.parse('/home/gasway/www/Gasway-WebService-Python-Flask-/app/static/prices.xml')
+    prices_tree = ET.parse('/home/gasway/Gasway-WebService-Python-Flask-/app/static/prices.xml')
     prices = prices_tree.getroot()
     for price in prices:
         if price.attrib in places_id:
